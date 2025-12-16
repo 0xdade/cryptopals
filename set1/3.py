79866b68c23c8b7eed12b4b776e539d6a179b3df
0xdade <0xdade@users.noreply.github.com>
2025-12-15 20:32:55 -0800

    Use NamedTuple instead to avoid magic positions

--- set1/3.py
+++ set1/3.py
@@ -1,7 +1,13 @@
 from binascii import hexlify
 import string
+from typing import NamedTuple
 
 INPUT_STR = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+class KeyScore(NamedTuple):
+    key: bytes
+    score: int
+    output: bytes
 
 def xor_equal_length(hex_str1: str, hex_str2: str) -> str:
     bytes1 = bytes.fromhex(hex_str1)
@@ -12,18 +18,18 @@ def xor_equal_length(hex_str1: str, hex_str2: str) -> str:
 def score_printable_ascii(instr: bytes) -> int:
     return sum([1 for x in instr if x in bytes(string.ascii_letters + " ,.\"'", "ascii")])
 
-def find_xor_key(input_str: str) -> list[tuple[bytes, int, bytes]]:
+def find_xor_key(input_str: str) -> list[KeyScore]:
     val = bytes.fromhex(input_str)
     scores = []
     for i in range(0xff):
         key = bytes([i] * len(val))
         result = xor_equal_length(INPUT_STR, hexlify(key).decode())
         score = score_printable_ascii(bytes.fromhex(result))
-        scores.append((key, score, bytes.fromhex(result)))
+        scores.append(KeyScore(key, score, bytes.fromhex(result)))
     return scores
 
 
 if __name__ == "__main__":
     scores = find_xor_key(INPUT_STR)
-    sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+    sorted_scores = sorted(scores, key=lambda x: x.score, reverse=True)
     print(sorted_scores[0:4])
--- set1/4.py
+++ set1/4.py
@@ -1,6 +1,13 @@
 from binascii import hexlify
 import string
 
+from typing import NamedTuple
+
+class KeyScore(NamedTuple):
+    key: bytes
+    score: int
+    output: bytes
+
 def xor_equal_length(hex_str1: str, hex_str2: str) -> str:
     bytes1 = bytes.fromhex(hex_str1)
     bytes2 = bytes.fromhex(hex_str2)
@@ -10,14 +17,14 @@ def xor_equal_length(hex_str1: str, hex_str2: str) -> str:
 def score_printable_ascii(instr: bytes) -> int:
     return sum([1 for x in instr if x in bytes(string.ascii_letters + " ,.\"'", "ascii")])
 
-def find_xor_key(input_str: str) -> list[tuple[bytes, int, bytes]]:
+def find_xor_key(input_str: str) -> list[KeyScore]:
     val = bytes.fromhex(input_str)
     scores = []
     for i in range(0xff):
         key = bytes([i] * len(val))
         result = xor_equal_length(input_str, hexlify(key).decode())
         score = score_printable_ascii(bytes.fromhex(result))
-        scores.append((key, score, bytes.fromhex(result)))
+        scores.append(KeyScore(key, score, bytes.fromhex(result)))
     return scores
 
 
@@ -27,7 +34,7 @@ if __name__ == "__main__":
     high_scores = []
     for input_str in input_lines:
         scores = find_xor_key(input_str.strip())
-        sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+        sorted_scores = sorted(scores, key=lambda x: x.score, reverse=True)
         high_scores.append(sorted_scores[0])
-    sorted_high_scores = sorted(high_scores, key=lambda x: x[1], reverse=True)
+    sorted_high_scores = sorted(high_scores, key=lambda x: x.score, reverse=True)
     print(sorted_high_scores[0:4])
